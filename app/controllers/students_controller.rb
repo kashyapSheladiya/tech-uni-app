e2434ef6e81f02f28b5af707a7d6b2cf16ba7150
kashyapSheladiya <kashyap27041993@gmail.com>
2019-05-20 14:07:13 +0200

    DRY (don't repeat yourself) code

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,10 +1,10 @@
 class StudentsController < ApplicationController
+  before_action :set_student, only: [:show, :edit, :update]
   def index
     @students = Student.all
   end
 
   def show
-    @student = Student.find(params[:id])
   end
 
   def new
@@ -15,18 +15,16 @@ class StudentsController < ApplicationController
     @student = Student.new(students_params)
     if @student.save
       flash[:notice] = "You have successfully signed up."
-      redirect_to root_path
+      redirect_to @student
     else
       render 'new'
     end
   end
 
   def edit
-    @student = Student.find(params[:id])
   end
 
   def update
-    @student = Student.find(params[:id])
     if @student.update(students_params)
       flash[:notice] = "Successfully Updated"
       redirect_to @student
@@ -39,5 +37,9 @@ class StudentsController < ApplicationController
 
   def students_params
     params.require(:student).permit(:name, :email)
+  end
+
+  def set_student
+    @student = Student.find(params[:id])
   end
 end
