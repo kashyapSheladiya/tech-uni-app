aeaf9fcca28d35955866b161693b1442c5186765
kashyapSheladiya <kashyap27041993@gmail.com>
2019-05-20 13:45:26 +0200

    Student update profile

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -21,6 +21,20 @@ class StudentsController < ApplicationController
     end
   end
 
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    if @student.update(students_params)
+      flash[:notice] = "Successfully Updated"
+      redirect_to @student
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def students_params
