382a319429b617dd8416c4902792896989585c70
kashyapSheladiya <kashyap27041993@gmail.com>
2019-05-21 16:30:40 +0200

    Added secure password for students

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_20_081953) do
+ActiveRecord::Schema.define(version: 2019_05_21_142223) do
 
   create_table "courses", force: :cascade do |t|
     t.string "short_name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_05_20_081953) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
