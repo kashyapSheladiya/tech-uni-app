71460649beebcd6347c8d82dbe80dad98c22d1bc
kashyapSheladiya <kashyap27041993@gmail.com>
2019-05-21 17:46:05 +0200

    Create and Destroy session - authentication method

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -3,8 +3,20 @@ class LoginsController < ApplicationController
   end
 
   def create
+    student = Student.find_by(email: params[:logins][:email].downcase)
+    if student && student.authenticate(params[:logins][:password])
+      session[:student_id] = student.id
+      flash[:notice] = "Wel Come."
+      redirect_to student
+    else
+      flash[:notice] = "Invalid Credentials."
+      render 'new'
+    end
   end
 
   def destroy
+    session[:student_id] = nil
+    flash[:notice] = "Successfully logged out."
+    redirect_to root_path
   end
 end
