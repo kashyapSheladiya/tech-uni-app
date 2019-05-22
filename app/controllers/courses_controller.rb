908213106b42736a7842aac4b2c4061a1df54a70
kashyapSheladiya <kashyap27041993@gmail.com>
2019-05-22 15:23:20 +0200

    Restricting action and view from another student

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,8 @@
 class CoursesController < ApplicationController
+  skip_before_action :require_user, only: [:index]
+
   def index
+    @courses = Course.all
   end
 
   def new
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
+  skip_before_action :require_user, only: [:about]
+
   def about
   end
 end
